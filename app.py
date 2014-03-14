7279573d5e3a5a5cf104f071d8f6ce20fad07177
Andrew <caolegario@gmail.com>
2014-03-14 11:31:11 -0700

    tip cost

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
  
 app = Flask(__name__)      
  
@@ -8,7 +8,10 @@ def home():
 
 @app.route('/results', methods=['POST'])
 def results():
-  return render_template('results.html')
- 
+	meal_cost = request.form["meal_cost"]
+	tip_percentage = request.form["tip_percentage"]
+	tip_cost = int(meal_cost) * (int(tip_percentage)*.01)
+	return render_template('results.html', meal_cost=meal_cost, tip_percentage=tip_percentage, tip_cost=tip_cost)
+
 if __name__ == '__main__':
   	app.run(debug=True)
