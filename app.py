8584121349d9d2a6d4635aa91e347f8123b7853e
Andrew <caolegario@gmail.com>
2014-03-18 12:49:52 -0700

    negative number edge case

--- app.py
+++ app.py
@@ -9,12 +9,19 @@ def home():
 @app.route('/results', methods=['POST'])
 def results():
 	try:
-		meal_cost = request.form["meal_cost"]
-		tip_percentage = request.form["tip_percentage"]
+		meal_cost = int(request.form["meal_cost"])
+		tip_percentage = int(request.form["tip_percentage"])
 		tip_cost = int(meal_cost) * (int(tip_percentage)*.01)
-		return render_template('results.html', meal_cost=meal_cost, tip_percentage=tip_percentage, tip_cost=tip_cost)
+		try_again = "Please enter a number greater than 0"
+		if meal_cost <= 0 or tip_percentage <= 0:
+			return render_template('home.html', again=try_again)
+		else:
+			return render_template('results.html', meal_cost=meal_cost, tip_percentage=tip_percentage, tip_cost=tip_cost)
+
 	except ValueError:
 		try_again = "Please enter a valid number"
 		return render_template('home.html', again=try_again)
+	
+
 if __name__ == '__main__':
   	app.run(debug=True)
